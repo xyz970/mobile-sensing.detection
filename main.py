6750103b9eddbd9a93dbeaf9073b08d5f22229f2
mhmmdadisptro <muhammadxxz7@gmail.com>
2024-06-02 13:32:29 +0700

    push

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,jsonify
+from werkzeug.utils import secure_filename
+from datetime import datetime
+import uuid
+from PIL import Image
+from tensorflow.keras.models import load_model
 import os
 import base64
 import requests
 import numpy as np
 import io, base64
-from PIL import Image
 import cv2
 import matplotlib.pyplot as plt
-from tensorflow.keras.models import load_model
+from predict import sequence_prediction
 
 app = Flask(__name__)
 app.debug = True
 
 
-@app.route("/")
+@app.route("/",methods=['POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        file = request.files['video']
+        filename = secure_filename(file.filename)
+        extension = filename.split('.')[1]
+        newFilename = f"{str(uuid.uuid4())}.{extension}"
+        folderPath = 'datasets/'+newFilename
+        file.save(folderPath)
+        return sequence_prediction(folderPath)
+
 
 
 @app.route('/predict/image', methods=['POST'])
