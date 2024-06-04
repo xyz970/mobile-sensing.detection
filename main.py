14fa9e57ab6a01ba3230bd7651f5922de4f060b9
mhmmdadisptro <muhammadxxz7@gmail.com>
2024-06-05 00:38:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,118 @@ from werkzeug.utils import secure_filename
 from datetime import datetime
 import uuid
 import os
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+import keras
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import cv2
 import base64
 import requests
-import io, base64
-import cv2
-import matplotlib.pyplot as plt
-from .predict import sequence_prediction
+# from .predict import sequence_prediction
 
 app = Flask(__name__)
 app.debug = True
+
+
+IMG_SIZE = 224
+BATCH_SIZE = 64
+EPOCHS = 10
+
+MAX_SEQ_LENGTH = 40
+NUM_FEATURES = 2048
+
+def build_feature_extractor():
+    feature_extractor = keras.applications.InceptionV3(
+        weights="imagenet",
+        include_top=False,
+        pooling="avg",
+        input_shape=(IMG_SIZE, IMG_SIZE, 3),
+    )
+    preprocess_input = keras.applications.inception_v3.preprocess_input
+
+    inputs = keras.Input((IMG_SIZE, IMG_SIZE, 3))
+    preprocessed = preprocess_input(inputs)
+
+    outputs = feature_extractor(preprocessed)
+    return keras.Model(inputs, outputs, name="feature_extractor")
+
+
+feature_extractor = build_feature_extractor()
+
+
+def prepare_single_video(frames):
+    frames = frames[None, ...]
+    frame_mask = np.zeros(
+        shape=(
+            1,
+            MAX_SEQ_LENGTH,
+        ),
+        dtype="bool",
+    )
+    frame_features = np.zeros(shape=(1, MAX_SEQ_LENGTH, NUM_FEATURES), dtype="float32")
+
+    for i, batch in enumerate(frames):
+        video_length = batch.shape[0]
+        length = min(MAX_SEQ_LENGTH, video_length)
+        for j in range(length):
+            frame_features[i, j, :] = feature_extractor.predict(batch[None, j, :])
+        frame_mask[i, :length] = 1  # 1 = not masked, 0 = masked
+
+    return frame_features, frame_mask
+
+
+
+def crop_center_square(frame):
+    y, x = frame.shape[0:2]
+    min_dim = min(y, x)
+    start_x = (x // 2) - (min_dim // 2)
+    start_y = (y // 2) - (min_dim // 2)
+    return frame[start_y : start_y + min_dim, start_x : start_x + min_dim]
+
+
+def load_video(path, max_frames=0, resize=(IMG_SIZE, IMG_SIZE)):
+    cap = cv2.VideoCapture(path)
+    frames = []
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame = crop_center_square(frame)
+            frame = cv2.resize(frame, resize)
+            frame = frame[:, :, [2, 1, 0]]
+            frames.append(frame)
+
+            if len(frames) == max_frames:
+                break
+    finally:
+        cap.release()
+    return np.array(frames)
+
+
+def sequence_prediction(path):
+    label = [0,1]
+    # class_vocab = label_processor.get_vocabulary()
+
+    frames = load_video(path)
+    frame_features, frame_mask = prepare_single_video(frames)
+    # probabilities = sequence_model.predict([frame_features, frame_mask])[0]
+    model = tf.keras.models.load_model('cnn_rnn.h5')
+    probabilities = model.predict([frame_features, frame_mask])[0]
+
+    for i in np.argsort(probabilities)[::-1]:
+        print(f"  {label[i]==0}: {probabilities[i] * 100:5.2f}%")
+    
+    print(f"Sorted  {label[0]}: {probabilities[0] * 100:5.2f}%")
+    print(f"Non floated Sorted  {label[0]}: {probabilities[0] * 100}%")
+    data = {
+        'probabilities': f"{probabilities[0] * 100:5.2f}",
+        'label': label[0],
+    }
+    return jsonify({'data':data})
+    # return frames
 
 
 @app.route("/",methods=['POST'])
