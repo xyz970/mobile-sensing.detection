6d2290b524cb7eb66327d1a8741adf5fdd2411ee
mhmmdadisptro <muhammadxxz7@gmail.com>
2024-06-05 20:39:48 +0700

    push

---
+++ convert.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+model = tf.keras.models.load_model('cnn_rnn.h5')
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+open("converted_model.tflite", "wb").write(tflite_model)
